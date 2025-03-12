7fa1577c91ef2166a79081c2250d7571976e785d
Rabia waheed <Ahtishamwaheed0786@gmail.com>
2025-03-12 13:23:21 +0500

    upgrade

--- unit_converter.py
+++ unit_converter.py
@@ -10,23 +10,25 @@ def convert_units(value, unit_from, unit_to):
     }
 
     key = f"{unit_from}_{unit_to}"  # Generate a key based on the input and output units
-
+     #logic to convert units
     if key in conversions:
         conversion = conversions[key]
         # if the conversion factor exists, use it to convert the value
         return value * conversion
     else:
-        return "Conversion not supported"
+        return "Conversion not supported" #return a message if the conversion is not suported
 
-# Streamlit UI code
+
 st.title("Unit Converter")# set the title of the web app
 # user input:numerical value to convert
-value = st.number_input("Enter the value:")
+value = st.number_input("Enter the value:",main_value=0)
 #dropdown to select unit to convert from
 unit_from = st.selectbox("Convert from:", ["meters", "kilometers", "grams", "kilograms"])
+#dropdown to select unit to convert tp
 unit_to = st.selectbox("Convert to:", ["meters", "kilometers", "grams", "kilograms"])
 
+# button to trigger the conversion
 if st.button("Convert"):
-    result = convert_units(value, unit_from, unit_to)
-    st.write(f"Converted Value: {result}")
+    result = convert_units(value, unit_from, unit_to) #  call the conversion function
+    st.write(f"Converted Value: {result}") #display the  result
 
